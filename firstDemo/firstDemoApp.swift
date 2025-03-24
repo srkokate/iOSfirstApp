7f84523a4927154dccd545ce9ce4ae61e27979a5
srkokate <srkokate.297@gmail.com>
2025-03-23 20:01:57 -0500

    Initial Commit

---
+++ firstDemo/firstDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstDemoApp.swift
+//  firstDemo
+//
+//  Created by Shubham Kokate on 3/23/25.
+//
+
+import SwiftUI
+
+@main
+struct firstDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
