a018600d8eeec5ad235ca20863d7fd187443b7ec
srkokate <srkokate.297@gmail.com>
2025-03-24 19:54:05 -0500

    update: view update

--- firstDemo/ContentView.swift
+++ firstDemo/ContentView.swift
@@ -9,10 +9,24 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Text("Hello Update")
-        }
-        .padding()
+        VStack(alignment: .leading, spacing: 20.0) {
+
+            Image("niagarafalls")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .cornerRadius(20)
+            HStack {
+                Text("Niagara Falls")
+                    .font(.title)
+                    .fontWeight(.bold)
+                Image(systemName: "star.fill")
+                Image(systemName: "star.fill")
+                Image(systemName: "star.fill")
+                Image(systemName: "star.fill")
+                Image(systemName: "star.leadinghalf.fill")
+            }
+            Text("Come experience the beauty of Niagara Falls!")
+        }.padding()
     }
 }
 
